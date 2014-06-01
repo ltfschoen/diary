3fcf51fff83fe5b64b1e9fd28b9fa6b1b281072c
ltfschoen <ltfschoen@gmail.com>
2014-06-02 09:34:28 +1000

    Added CoreDataStack Class to refactor and encapsulate everything to access Core Data

---
+++ Diary/CoreDataStack.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.h
+//  Diary
+//
+//  Created by Luke on 2/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataStack : NSObject
+
+@end
---
+++ Diary/CoreDataStack.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataStack.m
+//  Diary
+//
+//  Created by Luke on 2/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import "CoreDataStack.h"
+
+@implementation CoreDataStack
+
+@end

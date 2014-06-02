7093f88270de5f1669c5225a380a26fdd83d5328
ltfschoen <ltfschoen@gmail.com>
2014-06-03 09:15:21 +1000

    Added Segue to Modally presented ViewController Subclass. Created IBActions for Done and Cancel Buttons that call a dismissSelf method

---
+++ NewEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewEntryViewController.h
+//  Diary
+//
+//  Created by Luke on 3/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewEntryViewController : UIViewController
+
+@end

5267d303b1ed86a39993366e8899f40bebedfbf2
ltfschoen <ltfschoen@gmail.com>
2014-06-14 14:42:25 +1000

    Created Custom Class for EntryListTableViewController. Added Fetch Request Method to retrieve data

---
+++ EntryListTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  EntryListTableViewController.h
+//  Diary
+//
+//  Created by Luke on 14/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoreDataStack.h"
+
+@interface EntryListTableViewController : UITableViewController
+
+@end
---
+++ EntryListTableViewController.m
@@ -0,0 +1,133 @@
+//
+//  EntryListTableViewController.m
+//  Diary
+//
+//  Created by Luke on 14/06/2014.
+//  Copyright (c) 2014 Ceenos. All rights reserved.
+//
+
+#import "EntryListTableViewController.h"
+
+@interface EntryListTableViewController ()
+
+@end
+
+@implementation EntryListTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+#pragma mark - Fetch method
+
+// use method to create a fetch request so the fetch request can be tested independently of actually fetching data
+- (NSFetchRequest *)entryListFetchRequest {
+    
+    // create and return homogenous fetch request (i.e. need more than one fetch request to retrieve multiple entries)
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"KPInfoFeedEntry"];
+    
+    // create an array of sort descriptors with only one entry (date) in the array (no predicate required)
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:NO]];
+    
+    return fetchRequest;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
